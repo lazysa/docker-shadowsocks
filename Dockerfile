4a55e3d3273d9d3d0a9a941759dd8d833cb06ce0
win7@office <chenxu@dichen.store>
2018-04-18 18:52:19 +0800

    Add function

--- Dockerfile
+++ Dockerfile
@@ -3,23 +3,27 @@ LABEL maintainer="linuxjosery@gmail.com"
 LABEL comment="use to create a VPN by shadowsocks"
 ARG TZ='Asia/Shanghai'
 
-ENV HOST_NAME='vps-srv-01'
+ENV HOST_NAME='docker_shadowsocks'
 ENV TZ='Asia/Shanghai'
+ENV SS_PORT='8989'
+ENV SS_PASSWORD='PASSWORD' 
 ENV ELREPO_URL='https://www.elrepo.org/RPM-GPG-KEY-elrepo.org'
 ENV ELREPO_FILE='http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm'
 
 WORKDIR /root
 USER root
 # Create json config
+sed -i.bak -e "s/8989/${SS_PORT}/" -e "s/PASSWORD/${SS_PASSWORD}/" shadowsocks.json
 COPY shadowsocks.json /etc/shadowsocks.json
-EXPOSE 8989
+EXPOSE 8989 1080
 RUN yum install -y wget lsof \
    && echo ${HOST_NAME} > /etc/hostname \
    && \cp -a /usr/share/zoneinfo/${TZ} /etc/localtime \
+   # Install python-pip
    && wget https://bootstrap.pypa.io/get-pip.py \
    && python get-pip.py \
+   # Install shadowsocks
    && pip install shadowsocks \
-   # Add auto-start
    && /usr/bin/ssserver -c /etc/shadowsocks.json -d start \
    && echo -e '#Start the `shadowsocks` server\n\
     /usr/bin/ssserver -c /etc/shadowsocks.json -d start #>> /var/log/shadowsocks.log 2>&1' \
--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ yum-config-manager     --add-repo     https://download.docker.com/linux/centos/d
 yum-config-manager --disable docker-ce-edge
 yum install docker-ce -y
 
+
 # Disable ssh password login
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCmKUOBHFOugVz5pKoQB+OKUcItEfoBokEGzY+xYmQ+ilctGkrtqeQLZa4oQMoe72D6703Q0JgbtGdtbqqXD+UuTUn+XLj0r0t81z0QLGHX70qTZ9N6/GqxcaKFkCWCXorFaCuAAzQvlyjX/qPe2GU/8/R9ppI6S8g/V17qvNezCuk2qXBt0FqFXslKXAt7s+g/AMs/wF96qiPIXv17ITq39E16F53VtJgJX01IAPOOOvlLhgYvZzkyZ5Crv9xXu6NIT0ret4XOB2q6wJZSICx331DkUeJ+3NBT6w4Zb2abcyQhVmXMCuRlbFCfP23d8877LxUQFIdcu5+gqsuzrKOP Josery@MBP' > /root/.ssh/authorized_keys 
+read -p 'Please enter your ssh public key:' SSH_KEY 
+echo "$SSH_KEY" > /root/.ssh/authorized_keys 
 sed -i.bak  '/^PasswordAuthentication/s/yes/no/' /etc/ssh/sshd_config
 systemctl restart sshd.service
 
@@ -16,22 +18,29 @@ systemctl restart sshd.service
 \cp -a /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 iptables -A IN_public_allow -p tcp -m tcp --dport 8989 -j ACCEPT
 
+
 # Install google bbr
-# If upgrade kernel, startup script will be unavailable
-# https://www.vultr.com/docs/how-to-deploy-google-bbr-on-centos-7
+function Install_Kernel () 
+{
+    # If upgrade kernel, startup script will be unavailable
+    # https://www.vultr.com/docs/how-to-deploy-google-bbr-on-centos-7
+    rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
+    rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+    yum --enablerepo=elrepo-kernel install kernel-ml -y
+    #egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
+    grub2-set-default 0
+    shutdown -r now
+}
 
-#rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
-#rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-#yum --enablerepo=elrepo-kernel install kernel-ml -y
-##egrep ^menuentry /etc/grub2.cfg | cut -f 2 -d \'
-#grub2-set-default 0
-##shutdown -r now
 
-# Enable BBR
-echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf
-echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
-#lsmod | grep bbr
+function On_BBR ()
+{
+    # Enable BBR
+    echo 'net.core.default_qdisc=fq' | sudo tee -a /etc/sysctl.conf
+    echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.conf
+    sudo sysctl -p
+    #lsmod | grep bbr
+}
 
 # Install shadowsocks
 #yum -y install python-setuptools lrzsz vim lsof && easy_install pip
